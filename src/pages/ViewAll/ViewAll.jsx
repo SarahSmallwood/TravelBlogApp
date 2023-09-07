10c8d52b790e9636df3faf62b7081c2287f711c5
SarahSmallwood <133904630+SarahSmallwood@users.noreply.github.com>
2023-09-07 14:47:41 +0200

    final

--- src/pages/ViewAll/ViewAll.jsx
+++ src/pages/ViewAll/ViewAll.jsx
@@ -1,10 +1,11 @@
 import react from 'react';
 import ListPosts from "../../components/ListPosts/ListPosts";
+import styles from './ViewAll.modules.css';
 
 export default function ViewAll(posts) {
     return (
         <>
-        <h1>View All Travel Blogs</h1>
+        <h1 className='head'>View All Travel Blogs</h1>
         <ListPosts posts={posts}/>
         </>
     )
